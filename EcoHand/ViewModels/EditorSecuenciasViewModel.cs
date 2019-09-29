be6a51edf98d5bb44b808c0b027dffe8e8c2c13d
PabloAvalo <pabloavalo@outlook.com>
2019-09-29 20:33:56 -0300

    Maqueta de diseñador de secuencia

--- EcoHand/ViewModels/EditorSecuenciasViewModel.cs
+++ EcoHand/ViewModels/EditorSecuenciasViewModel.cs
@@ -6,6 +6,8 @@ using System.Text;
 using System.Threading.Tasks;
 using EcoHand.EventModels;
 using System.ComponentModel;
+using EcoHand.Models;
+using EcoHand.Handlers;
 
 namespace EcoHand.ViewModels
 {
@@ -17,23 +19,85 @@ namespace EcoHand.ViewModels
         public EditorSecuenciasViewModel(IEventAggregator events)
         {
             _events = events;
+            Secuencia = new BindingList<GestoModel>();
         }
 
-        private BindingList<string> _secuencia;
+        protected override async void OnViewLoaded(object view)
+        {
+            base.OnViewLoaded(view);
+            await CargarListaDeGestosAsync();
+         
+        }
+
+        private async Task CargarListaDeGestosAsync()
+        {
+            var resp = await GestoHandler.ObtenerListaDeGestosAsync();
+            Gestos = new BindingList<GestoModel>();
+            foreach (var item in resp)
+            {
+                Gestos.Add(new GestoModel() { Id = item.ID, Nombre = item.Nombre });
+            }
+        }
+
+        //deberia ser de gestos y eventos
+        private BindingList<GestoModel> _secuencia;
+
+        public BindingList<GestoModel> Secuencia
+        {
+            get { return _secuencia; }
+            set
+            {
+                _secuencia = value;
+                NotifyOfPropertyChange(() => Secuencia);
+
+            }
+        }
+
+
+        private BindingList<GestoModel> _gestos;
+
+        public BindingList<GestoModel> Gestos
+        {
+            get { return _gestos; }
+            set
+            {
+                _gestos = value;
+                NotifyOfPropertyChange(() => Gestos);
+
+            }
+        }
+
+        private GestoModel _selectedGesto;
+
+        public GestoModel SelectedGesto
+        {
+            get
+            {
+                return _selectedGesto;
+            }
+            set
+            {
+                _selectedGesto = value;
+                NotifyOfPropertyChange(() => SelectedGesto);
+                
+            }
+        }
+
 
         public void AgregarASecuencia()
         {
-
+            this.Secuencia.Add(SelectedGesto);
         }
 
         public void EliminarDeSecuencia()
         {
-
+            this.Secuencia.RemoveAt(Secuencia.Count);
+            NotifyOfPropertyChange(() => Secuencia);
         }
 
         public void GuardarSecuencia()
         {
-
+            //logica para crear secuencia
         }
 
 
